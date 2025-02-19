cfb712bf9fe667b39288d5b0192228dcdebfa37d
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-19 00:10:33 -0300

    feat: adicionado salvar arquivo json

--- src/tasks.py
+++ src/tasks.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import datetime
+import os
+import json
 
 from prefect import task
 
@@ -31,3 +33,22 @@ def gerar_nome_arquivo() -> str:
     nome_arquivo = f"gps_brt_{timestamp}.json"
     print(nome_arquivo)
     return nome_arquivo
+
+@task
+def salva_arquivo_json(nome_arquivo, dados):
+    """
+    Salva os dados extraídos da API em um arquivo JSON no diretório especificado.
+
+    O nome do arquivo é gerado dinamicamente com a função gerar nome.
+    Caso ocorra um erro durante a gravação, uma mensagem de erro será exibida.
+
+    """
+
+    caminho_arquivo = os.path.join("raw", nome_arquivo)
+
+    try:
+        with open(caminho_arquivo, "w", encoding="utf-8") as file:
+            json.dump(dados, file, ensure_ascii=False, indent=4)
+            print(f"Dados salvos no arquivo '{caminho_arquivo}' com sucesso!")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo JSON: {e}")
