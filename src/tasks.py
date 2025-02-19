04c179bf3f538ab99820ab9286397719aecdc153
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-19 00:08:31 -0300

    feat: adicionado gerador nome de arquivo

--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import datetime
 
 from prefect import task
 
@@ -13,3 +14,20 @@ def request():
     print(response.json())
     return response.json()
 
+
+@task
+def request():
+
+    base_url = "https://dados.mobilidade.rio/gps/brt"
+    url = base_url
+    response = requests.get(url)
+    response.raise_for_status()
+    print(response.json())
+    return response.json()
+
+@task
+def gerar_nome_arquivo() -> str:
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") 
+    nome_arquivo = f"gps_brt_{timestamp}.json"
+    print(nome_arquivo)
+    return nome_arquivo
