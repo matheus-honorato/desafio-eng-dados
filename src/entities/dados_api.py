a7d5ad4cf7461315c214ca06a57273b52d4bc1c8
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-19 00:39:56 -0300

    feat: adicionado entidade

---
+++ src/entities/dados_api.py
@@ -0,0 +1,15 @@
+from configs.base import Base
+from sqlalchemy import Column, Integer, Text, TIMESTAMP, func
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class Dados_api(Base):
+    __tablename__ = "dados_api"
+    __table_args__ = {"schema": "raw_brt"}  # Definir o schema
+
+    id = Column(Integer, primary_key=True)
+    data_extracao = Column(TIMESTAMP, server_default=func.now())  # Timestamp padrão
+    dados = Column(JSONB, nullable=False)  # JSON armazenado no formato otimizado do PostgreSQL
+
+    def __repr__(self):
+        return f"Dados [Origem={self.origem}, dados={self.dados}]"
