f9b4438ff90efa7d514e3e8f6e3f50900e0a160b
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-20 17:48:17 -0300

    docs: adicionado docstring

--- src/entities/dados_api.py
+++ src/entities/dados_api.py
@@ -4,9 +4,19 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 
 class Dados_api(Base):
+     """
+    Classe que representa a tabela 'dados_api' no banco de dados do POSTGRESQL.
+
+    A tabela é responsável por armazena os dados extraídos da API.
+
+    Attributes:
+        id (int): Chave primária da tabela.
+        data_extracao (datetime): Timestamp da extração dos dados (gerado automaticamente).
+        dados (JSONB): Dados da API armazenados em formato JSON.
+    """
     __tablename__ = "dados_api"
-    __table_args__ = {"schema": "raw_brt"}  # Definir o schema
+    __table_args__ = {"schema": "raw_brt"}  
 
     id = Column(Integer, primary_key=True)
-    data_extracao = Column(TIMESTAMP, server_default=func.now())  # Timestamp padrão
-    dados = Column(JSONB, nullable=False)  # JSON armazenado no formato otimizado do PostgreSQL
+    data_extracao = Column(TIMESTAMP, server_default=func.now())  
+    dados = Column(JSONB, nullable=False) 
