669998cb73484905df753c538e3e9250ea9b87f3
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-20 02:56:34 -0300

    feat: adicionado fluxo schema e tabela

--- src/flows.py
+++ src/flows.py
@@ -1,7 +1,7 @@
 from prefect import task, Flow
 import prefect
 from prefect.schedules import IntervalSchedule
-from tasks import request, gerar_nome_arquivo, salva_arquivo_json, inserir_dados_db, carregar_dados_json
+from tasks import request, gerar_nome_arquivo, salva_arquivo_json, inserir_dados_db, carregar_dados_json, criar_schema, criar_table
 from datetime import timedelta, datetime
 
 
@@ -11,11 +11,14 @@ schedule = IntervalSchedule(
 
 )
 
-
 with Flow("BRT-ELT", schedule) as flow:
     dados_requisicao = request()
     nome_arquivo = gerar_nome_arquivo()
     caminho_arquivo = salva_arquivo_json(nome_arquivo, dados_requisicao)
     carga_dados = carregar_dados_json(caminho_arquivo)
+    criando_schema = criar_schema()
+    criando_tabela = criar_table()
     inserir_no_banco = inserir_dados_db(carga_dados)
 
+
+# flow.register(project_name="teste", labels=["development"])
