83b0322124528b24881dcd47bc4b0a87c26bffba
Matheus Honorato <matheuswebmw@gmail.com>
2025-02-20 17:58:04 -0300

    docs: adicionado docstring

--- src/repository/dados_apiRepository.py
+++ src/repository/dados_apiRepository.py
@@ -6,23 +6,44 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 
 class Dados_apiRepository:
+    """
+    Classe responsável por realizar operações de banco de dados na tabela 'dados_api'
+
+    """
     def select(self):
+        """
+        Recupera todos os registros da tabela dados_api.
+
+        """
         with DBConnectionHandler() as db:
             data = db.session.query(Dados_api).all()
             return data
 
     def insert(self, dados):
+        """
+        Insere um novo registro na tabela dados_api
+
+        """
+
         with DBConnectionHandler() as db:
             data_insert = Dados_api(dados=dados)
             db.session.add(data_insert)
             db.session.commit()
 
     def executa_query(self, query):
+        """
+        Executa uma query SQL personalizada no banco de dados.
+
+        """
         engine = DBConnectionHandler().get_engine()
         with engine.begin() as conn:
             conn.execute(text(query))
 
     def create_table(self):
+        """
+        Cria a tabela dados_api no no schema raw_brt do banco de dados, caso ela não exista.
+
+        """
         db = DBConnectionHandler()
         engine = db.get_engine()
         metadata = MetaData()
